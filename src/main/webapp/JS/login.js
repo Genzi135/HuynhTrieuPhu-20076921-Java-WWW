16bf3096eb99fb65b4d2a5e89ef0f033c8bcd632
Genzi135 <phu13052002@gmail.com>
2023-09-15 21:50:51 +0700

    fix html login.js

--- src/main/webapp/JS/login.js
+++ src/main/webapp/JS/login.js
@@ -1,43 +1,47 @@
-document.getElementById("submit").addEventListener("click", (even) => {
-    even.preventDefault();
-    var params = {
-        action: "login",
-        us: document.getElementById("user").value,
-        pwd: document.getElementById("pwd").value,
-    };
-    var queryString = Object.keys(params)
-        .map((key) => key + "=" + encodeURIComponent(params[key]))
-        .join("&");
-    var apiUrl =
-        "http://localhost:8080/week01_demoProject/ControllerServlet";
-    var urlWithParams = apiUrl + "?" + queryString;
-    fetch(urlWithParams)
-        .then((response) => {
-            if (response.ok) {
-                return response.json();
-            } else {
-                alert("Login failed");
-                throw new Error("Request failed with status: " + response.status);
-            }
-        })
-        .then((data) => {
-            var role = JSON.parse(data.role);
-            var account = JSON.parse(data.account);
-            console.log(data);
-            console.log(role);
-            console.log(account);
-            if (role.role_id === "admin") {
-                alert("Login successfully as admin :" + account.full_name);
-                localStorage.clear();
-                localStorage.setItem("accLogin", JSON.stringify(account));
-                localStorage.setItem("roleLogin", JSON.stringify(role));
-                window.location.href = "dashboard.html";
-            } else {
-                alert("Login successfully as user : " + account.full_name);
-                localStorage.clear();
-                localStorage.setItem("accLogin", JSON.stringify(account));
-                localStorage.setItem("roleLogin", JSON.stringify(role));
-                window.location.href = "userInfo.html";
-            }
-        });
-});
+window.onload = function () {
+
+    document.getElementById('submit').addEventListener("click", (event) => {
+        even.preventDefault();
+        var params = {
+            action: "login",
+            us: document.getElementById("user").value,
+            pwd: document.getElementById("pwd").value,
+        };
+        var queryString = Object.keys(params)
+            .map((key) => key + "=" + encodeURIComponent(params[key]))
+            .join("&");
+        var apiUrl =
+            "http://localhost:8080/week01_demoProject/ControllerServlet";
+        var urlWithParams = apiUrl + "?" + queryString;
+        fetch(urlWithParams)
+            .then((response) => {
+                if (response.ok) {
+                    return response.json();
+                } else {
+                    alert("Login failed");
+                    throw new Error("Request failed with status: " + response.status);
+                }
+            })
+            .then((data) => {
+                var role = JSON.parse(data.role);
+                var account = JSON.parse(data.account);
+                console.log(data);
+                console.log(role);
+                console.log(account);
+                if (role.role_id === "admin") {
+                    alert("Login successfully as admin :" + account.full_name);
+                    localStorage.clear();
+                    localStorage.setItem("accLogin", JSON.stringify(account));
+                    localStorage.setItem("roleLogin", JSON.stringify(role));
+                    window.location.href = "dashboard.html";
+                } else {
+                    alert("Login successfully as user : " + account.full_name);
+                    localStorage.clear();
+                    localStorage.setItem("accLogin", JSON.stringify(account));
+                    localStorage.setItem("roleLogin", JSON.stringify(role));
+                    window.location.href = "user_info.html";
+                }
+            });
+    });
+
+};
