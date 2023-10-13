20b4fc7906c291637d7890137609b620bc2cbbe5
Genzi135 <phu13052002@gmail.com>
2023-10-13 17:06:38 +0700

    drive demo code in AddressRepository

--- src/main/java/com/iuh/edu/vn/week4/repositories/AddressRepository.java
+++ src/main/java/com/iuh/edu/vn/week4/repositories/AddressRepository.java
@@ -3,5 +3,10 @@ package com.iuh.edu.vn.week4.repositories;
 import com.iuh.edu.vn.week4.models.Address;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AddressRepository extends JpaRepository<Address, Long> {
+    Address findAddressById(String id);
+
+    List<Address> getTopByCity(int top, String city);
 }
